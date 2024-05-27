6990d684a734f1f028f01ea0aaed47149b9e849c
cleytonVale <cleyton.vale@hotmail.com>
2024-05-26 22:00:37 -0300

    Inserção do primeiro arquivo no github

---
+++ cuidados_bebe.py
@@ -0,0 +1,22 @@
+class bebe:
+    def __init__(self, nome, data_nascimento, peso, altura, grupo_sanguineo)
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.peso = peso
+        self.altura = altura
+        self.grupo_sanguineo = grupo_sanguineo
+
+class Alimentacao:
+    def __init__(self, data_hora, tipo, quantidade)
+        self.data_hora = data_hora
+        self.tipo = tipo
+        self.quantidade  = quantidade
+
+class TrocaDeFralda:
+    pass
+
+class ConsultaMedica:
+    pass
+
+class RegistrosCuidados:
+    
